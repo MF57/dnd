e22d0dc2472265867f44faf4d041d9155a8c9cd0
Piotr Bochenek <p.bochenek@avsystem.com>
2019-07-13 17:25:02 +0200

    first inputs in session storage

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import {MDCNotchedOutline} from '@material/notched-outline';
 import {MDCFormField} from '@material/form-field';
 import {MDCCheckbox} from '@material/checkbox';
 
-const textFields = [
+const textFieldIds = [
     "#character-name-textfield",
     "#player-name-textfield",
     "#class-textfield",
@@ -39,12 +39,55 @@ const notches = [
     "#skin-outline",
 ];
 
-textFields.forEach(textField => new MDCTextField(document.querySelector(textField)));
+textFieldIds.forEach(textField => new MDCTextField(document.querySelector(textField)));
 notches.forEach(notch => new MDCNotchedOutline(document.querySelector(notch)));
 
 
 const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'));
 const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
 formField.input = checkbox;
+
+
+let character = {};
+if (sessionStorage.character) {
+    character = JSON.parse(sessionStorage.character)
+}
+
+
+function registerCharacterInput(id, characterUpdater, characterValueGetter) {
+    const input = document.getElementById(id);
+    const textfield = document.getElementById(id + "-textfield");
+    if (textfield) {
+        const mdc = new MDCTextField(document.getElementById(id + "-textfield"));
+        if (characterValueGetter(character)) {
+            mdc.value = characterValueGetter(character);
+        }
+    } else {
+        if (characterValueGetter(character)) {
+            input.value = characterValueGetter(character)
+        }
+    }
+    input.addEventListener("blur", () => {
+        characterUpdater(input.value);
+        sessionStorage.character = JSON.stringify(character);
+        console.log(character);
+    })
+}
+
+registerCharacterInput("character-name", (value) => character.characterName = value, (character) => character.characterName);
+registerCharacterInput("player-name", (value) => character.playerName = value, (character) => character.playerName);
+registerCharacterInput("class", (value) => character.class = value, (character) => character.class);
+registerCharacterInput("level", (value) => character.level = value, (character) => character.level);
+registerCharacterInput("race", (value) => character.race = value, (character) => character.race);
+registerCharacterInput("size", (value) => character.size = value, (character) => character.size);
+registerCharacterInput("character", (value) => character.character = value, (character) => character.character);
+registerCharacterInput("sex", (value) => character.sex = value, (character) => character.sex);
+registerCharacterInput("age", (value) => character.age = value, (character) => character.age);
+registerCharacterInput("height", (value) => character.height = value, (character) => character.height);
+registerCharacterInput("weight", (value) => character.weight = value, (character) => character.weight);
+registerCharacterInput("eye", (value) => character.eye = value, (character) => character.eye);
+registerCharacterInput("hair", (value) => character.hair = value, (character) => character.hair);
+registerCharacterInput("skin", (value) => character.skin = value, (character) => character.skin);
+registerCharacterInput("religion", (value) => character.religion = value, (character) => character.religion);
 
 
